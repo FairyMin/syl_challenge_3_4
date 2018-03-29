a35d2fff6b3f2b4fb22934319784a106d0aa30b6
mzg <15122302865@163.com>
2018-03-29 23:02:12 +0800

    未完成

--- challenge3_4.py
+++ challenge3_4.py
@@ -27,14 +27,24 @@ def open_parser(filename):
     return parsers
 
 def main():
-    #使用正则表达式解些日志文件
+    #使用正则表达式解析日志文件
     logs = open_parser('/home/shiyanlou/Code/nginx.log')
     '''
     1.解析文件就是分离不同类型数据(IP，时间，状态码等)
 
     2.从解析后的文件中统计挑战所需要的信息
     '''
-    pass
+    #ip_dict 包含的信息是2017年1月11日期间访问此时最多的IP以及对应的访问次数
+    #格式{'IP':'times'}
+    ip_dict = {}
+    
+    #url_dict包含的信息是 GET请求状态为404最多的URL 请求地址以及请求次数
+    #格式 {"IP":"times"}
+    url_dic = {}
+    
+    for info in logs:
+        print(info[0],info[1])
+        break
 
     return ip_dict,url_dict
 
